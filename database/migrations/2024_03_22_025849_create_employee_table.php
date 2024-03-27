f6d555a8128bd03f236b57bd7364bfdca3739aa6
dienducminh <dienducminh2201@gmail.com>
2024-03-27 11:51:08 +0700

    second commit

--- database/migrations/2024_03_22_025849_create_employee_table.php
+++ database/migrations/2024_03_22_025849_create_employee_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('employee', function (Blueprint $table) {
             $table->id();
             $table->foreignId('UserID')->constrained('users')->onDelete('cascade');
-            $table->integer('user_level')->default('0');
+            $table->integer('role');
             $table->timestamps();
         });
     }
